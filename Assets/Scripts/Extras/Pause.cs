75049fc459ebb8681ec5710df7b3106495b647c3
Vivek Raman <vr.ac4bf@live.com>
2020-04-15 17:23:06 +0530

    Game can now be paused and resumed

---
+++ Assets/Scripts/Extras/Pause.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    [SerializeField] private bool canPause = true;
+    [SerializeField] private GameObject pausePanel = null;
+
+    private bool isPaused = false;
+
+    private void Update()
+    {
+        if (!canPause) return;
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            pausePanel.SetActive(isPaused.Toggle());
+            
+            if (!Cursor.visible)
+            {
+                UnlockMouse();
+            }
+            else
+            {
+                LockMouse();
+            }
+        }
+
+        Time.timeScale = isPaused ? 0f : 1f;
+    }
+    
+    private void UnlockMouse()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    private void LockMouse()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    
+}
